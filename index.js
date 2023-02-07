9ecdcd134a96f1f69cda1ec17f5512a26e70e9b2
Albert <albert.developer99@gmail.com>
2023-02-07 12:20:57 +0530

    after added front view

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ const bodyParser= require("body-parser");
 const axios = require('axios');
 const scan = require('./routes/scan');
 const helmet = require('helmet');
+const cors = require('cors');
 const port= process.env.port || 4000;
 const app= express();
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
+app.use(express.static('./public'))
 app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.sendFile('index.html',{root:__dirname})
+});
 
 // set security http headers
 app.use(helmet());
